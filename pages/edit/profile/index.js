0d56850025964d86618a863139bf9a41240a92ce
Alan <alan.fer09@gmail.com>
2022-05-26 16:58:18 -0300

    add animation to load

--- pages/edit/profile/index.js
+++ pages/edit/profile/index.js
@@ -5,10 +5,18 @@ import Button from "../../../components/Button";
 import { useEffect, useState } from "react";
 import { uploadImage } from "../../../firebase/client/client";
 import { updateUser } from "../../../firebase/client/auth";
+import CheckAnimation from "../../../components/CheckAnimation";
+import Spinner from "../../../components/Spinner/index";
+import { useRouter } from "next/router";
 export default function EditProfile() {
   const [file, setFile] = useState(null);
   const [imgURL, setImgURL] = useState(null);
   const [displayName, setDisplayName] = useState("");
+  const [finish, setFinish] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
   const handleName = (event) => {
     setDisplayName(event.target.value);
   };
@@ -25,21 +33,22 @@ export default function EditProfile() {
 
   const handleSumbit = async (event) => {
     event.preventDefault();
+    setLoading(true);
     try {
       const photoURL = await uploadImage({ file, folder: "profile-img" });
-      const updated = await updateUser({ photoURL, displayName });
-      console.log("updated", updated);
+      await updateUser({ photoURL, displayName });
+      setFinish(true);
     } catch (error) {
       console.log("error en handleSumbit", error);
     }
   };
 
-  return (
+  return !finish ? (
     <>
       <form onSubmit={handleSumbit}>
         <section>
           <label>
-            <span>elige una foto de perfil</span>
+            <span>Elige una foto de perfil</span>
             <img src={imgURL} />
             <div></div>
             <input
@@ -55,36 +64,45 @@ export default function EditProfile() {
             onChange={handleName}
           />
         </section>
-        <Button color={colors.primary} disabled={displayName === "" || !imgURL}>
-          crear perfil
-        </Button>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Button
+            color={colors.primary}
+            disabled={displayName === "" || !imgURL}
+          >
+            crear perfil
+          </Button>
+        )}
       </form>
       <style jsx>{`
-
+        span {
+          font-size: 15px;
+        }
         div {
-          width: 100px;
-          height: 100px;
-          background-color:#cac7c7;
+          width: 150px;
+          height: 150px;
+          background-color: #cac7c7;
           border-radius: 99999px;
           margin: 20px;
-          cursor:pointer;
-          display:${imgURL ? "none" : "block"};
-          
+          cursor: pointer;
+          display: ${imgURL ? "none" : "block"};
         }
-        img{
-        display:${imgURL ? "block" : "none"};
-        width: 100px;
-          height: 100px;
+        img {
+          display: ${imgURL ? "block" : "none"};
+          width: 150px;
+          height: 150px;
           border-radius: 99999px;
-          margin: 20px;
-          cursor:pointer;
+          margin: 30px;
+          cursor: pointer;
           object-fit: cover;
+          border: solid 3px ${colors.primary};
         }
-        img:hover{
-            filter: brightness(0.5);
+        img:hover {
+          filter: brightness(0.5);
         }
-        div:hover{
-            filter: brightness(0.5);
+        div:hover {
+          filter: brightness(0.5);
         }
         section {
           display: flex;
@@ -103,17 +121,55 @@ export default function EditProfile() {
           width: 165px;
         }
         label {
-          width: 165px;
+          width: auto;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
         }
         input[type="text"] {
           border: 0px;
-          border-bottom: solid gray 1px;
+          border-bottom: solid ${colors.primary} 3px;
           outline: none;
-          width:150px;
+          width: 150px;
+          font-size: 15px;
         }
         input[type="file"] {
-       display:none
+          display: none;
         }
+      `}</style>
+    </>
+  ) : (
+    <>
+      <div>
+        <section>
+          {" "}
+          <span>
+            ¡Felicidades!
+            <br /> Ya estas registrado
+          </span>
+          <div className="check_container">
+            <CheckAnimation />
+          </div>
+        </section>
+        <Button color={colors.primary} onClick={() => router.push("/home")}>
+          {" "}
+          ir al inicio
+        </Button>
+      </div>
+      <style jsx>{`
+        .check_container {
+          margin: 20px;
+          width: auto;
+          height: auto;
+        }
+        div {
+          text-align: center;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          width: 100vw;
+          height: 100vh;
+          flex-direction: column;
         }
       `}</style>
     </>
